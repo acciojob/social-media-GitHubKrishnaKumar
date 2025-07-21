4008bfff8587687e31f1b0bb15e423a058d5ec63
Krishna Kumar <86869782+GitHubKrishnaKumar@users.noreply.github.com>
2025-07-21 14:58:17 +0530

    Create PostList.js

---
+++ src/components/PostList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PostItem from './PostItem';
+
+export default function PostList({ posts = [] }) {
+  return (
+    <div className="posts-list">
+      {posts.map(post => (
+        <PostItem key={post.id} post={post} />
+      ))}
+    </div>
+  );
+}
